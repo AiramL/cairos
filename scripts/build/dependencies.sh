5b9aba3e1e7321862c8755b1f5772176c950aa39
AiramL <airam@poli.ufrj.br>
2026-03-24 19:56:11 -0300

    updating path script

--- scripts/build/dependencies.sh
+++ scripts/build/dependencies.sh
@@ -20,7 +20,7 @@ if ! command -v docker >/dev/null 2>&1; then
 	sudo apt-get update
 	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-	docker --version
+	sudo docker --version
 
 fi
 
--- scripts/build/paths.sh
+++ scripts/build/paths.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-[ ! -d data/processed/speed0 ] && mkdir -p data/processed/speed0
-[ ! -d data/processed/speed1 ] && mkdir -p data/processed/speed1
-[ ! -d data/processed/speed2 ] && mkdir -p data/processed/speed2
-[ ! -d data/raw/speed0 ] && mkdir -p data/raw/speed0
-[ ! -d data/raw/speed1 ] && mkdir -p data/raw/speed1
-[ ! -d data/raw/speed2 ] && mkdir -p data/raw/speed2
-[ ! -d mobility/processed/speed0 ] && mkdir -p mobility/processed/speed0
-[ ! -d mobility/processed/speed1 ] && mkdir -p mobility/processed/speed1
-[ ! -d mobility/processed/speed2 ] && mkdir -p mobility/processed/speed2
-[ ! -d mobility/raw/speed0 ] && mkdir -p mobility/raw/speed0
-[ ! -d mobility/raw/speed1 ] && mkdir -p mobility/raw/speed1
-[ ! -d mobility/raw/speed2 ] && mkdir -p mobility/raw/speed2
-[ ! -d results/client_selection/speed0 ] && mkdir -p results/client_selection/speed0
-[ ! -d results/client_selection/speed1 ] && mkdir -p results/client_selection/speed1
-[ ! -d results/client_selection/speed2 ] && mkdir -p results/client_selection/speed2
-[ ! -d results/client_selection/raw/epoch ] && mkdir -p results/client_selection/raw/epoch
-[ ! -d results/client_selection/processed ] && mkdir -p results/client_selection/processed
-[ ! -d results/selected_clients ] && mkdir -p results/selected_clients
+mapfile -t values < <(yq '.simulation.speed.value[]' config/config.yaml)
+mapfile -t indexes < <(yq '.simulation.speed.index[]' config/config.yaml)
+
+for i in "${!indexes[@]}"
+do
+
+	[ ! -d mobility/processed/speed"${values[$i]}" ] && mkdir -p mobility/processed/speed"${values[$i]}"
+	[ ! -d data/processed/speed"${values[$i]}" ] && mkdir -p data/processed/speed"${values[$i]}"
+	[ ! -d data/raw/speed"${values[$i]}" ] && mkdir -p data/raw/speed"${values[$i]}"
+	[ ! -d mobility/raw/speed"${values[$i]}" ] && mkdir -p mobility/raw/speed"${values[$i]}"
+
+done
+#[ ! -d results/client_selection/speed0 ] && mkdir -p results/client_selection/speed0
+#[ ! -d results/client_selection/speed1 ] && mkdir -p results/client_selection/speed1
+#[ ! -d results/client_selection/speed2 ] && mkdir -p results/client_selection/speed2
+#[ ! -d results/client_selection/raw/epoch ] && mkdir -p results/client_selection/raw/epoch
+#[ ! -d results/client_selection/processed ] && mkdir -p results/client_selection/processed
