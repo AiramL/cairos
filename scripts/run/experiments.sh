66d71d499c477ff147f01aa0081e390048cbd570
airaml <>
2026-03-31 17:29:23 +0000

    fixing speed bug

--- scripts/run/experiments.sh
+++ scripts/run/experiments.sh
@@ -13,12 +13,16 @@ alpha_dirichlet=$(yq '.simulation.federated_learning.data.alpha' config/config.y
 timeout=$(yq '.simulation.federated_learning.server.timeout' config/config.yaml )
 strategy=$(yq '.simulation.federated_learning.server.strategy' config/config.yaml )
 distribution_type=$(yq '.simulation.federated_learning.server.epochs_distribution' config/config.yaml )
-speed_id=$(yq '.simulation.speed.index' config/config.yaml )
+
+mapfile -t speed_ids < <(yq '.simulation.speed.index[]' config/config.yaml)
 
 exec_id=0
 
 echo "Starting FL training with $framework server at $ip:$port, $n_clients clients executing $local_epochs local epochs, for $rounds rounds, selecting $n_clients_fit clients to fit the $model model for $local_epochs local epochs on the dataset $dataset using a datadistribution with alpha equals to $alpha_dirichlet, a maximun timeout of $timeout seconds, using the $strategy strategy, the $distribution_type local epochs distribution, and speed index $speed_id."
 
+for speed_id in speed_ids;
+do
+
 source scripts/run/baremetal.sh "$strategy" "$alpha_dirichlet" "$model" "$port" "$framework" "$n_clients" "$dataset" "$rounds" "$local_epochs" "$n_clients_fit" "$distribution_type" "$timeout" "$speed_id" "$exec_id"
 	
-
+done
