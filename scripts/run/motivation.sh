13e6a38d3e2165557c82ef0783abefff8ce236ce
AiramL <airam@poli.ufrj.br>
2026-08-04 09:03:26 -0300

    adding new motivation experiments

--- scripts/run/motivation.sh
+++ scripts/run/motivation.sh
@@ -1,33 +1,60 @@
 #!/bin/bash
 
-alpha_dirichlet="5.0"
-global_epochs=15
-n_clients=50
+framework=$(yq '.simulation.federated_learning.framework' config/config.yaml )
+framework=$(echo "$framework" | tr -d '"')
 
+port=$(yq '.simulation.federated_learning.server.port' config/config.yaml )
+port=$(echo "$port" | tr -d '"')
 
-for fit in 1 25 50 10;
+n_clients=$(yq '.simulation.cars' config/config.yaml )
+n_clients=$(echo "$n_clients" | tr -d '"')
+
+rounds=$(yq '.simulation.federated_learning.server.rounds' config/config.yaml )
+rounds=$(echo "$rounds" | tr -d '"')
+
+base_station_range=$(yq '.simulation.base_station.range' config/config.yaml ) 
+base_station_range=$(echo "$base_station_range" | tr -d '"')
+
+fit=$(yq '.simulation.federated_learning.server.n_clients_fit' config/config.yaml )
+fit=$(echo "$fit" | tr -d '"')
+
+model=$(yq '.simulation.federated_learning.client.model' config/config.yaml )
+model=$(echo "$model" | tr -d '"')
+
+distribution_type=$(yq '.simulation.federated_learning.server.epochs_distribution' config/config.yaml )
+distribution_type=$(echo "$distribution_type" | tr -d '"')
+
+speed_id=0
+data_type="delay"
+data_type=$(echo "$data_type" | tr -d '"')
+
+for dataset in "CIFAR-10" "SIGN"; 
 do
-for local_epochs in 5;
+
+for alpha_dirichlet in 0.5 1.0 5.0;
 do
 
-	for distribution_type in "equal";
-	do
+for local_epochs in 10 5 20;
+do
 
-		for dataset in "CIFAR-10"; 
-		do
-			for timeout in 2000;
-			do
+for fit in 1 2 5 10 20 40;
+do
 
-				for strategie in "fedavg";
-				do
-					source scripts/run/baremetal.sh "$strategie" "$alpha_dirichlet" "RESNET10" "8081" "torch" "$n_clients" "$dataset" "$global_epochs" "$local_epochs" "$fit" "$distribution_type" "$timeout" "$timeout"
+for timeout in 1000;
+do
+
+for index in 0 1 2; 
+do
+
+for strategie in "fedavg";
+do
+
+source scripts/run/baremetal.sh "$strategie" "$alpha_dirichlet" "$model" "$port" "$framework" "$n_clients" "$dataset" "$rounds" "$local_epochs" "$fit" "$distribution_type" "$timeout" "$speed_id" "$index" "$base_station_range" "$data_type"
 					
-				done
-			done
-
-		done
-
-	done
-
+done
+done
+done
+done
+done
 done
 done
